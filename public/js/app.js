6b29c2d7a31d0db558eeaaf41964bcecea0f60a5
Onkar Patil <onkarvgpatil1998@gmail.com>
2024-09-17 00:25:19 +0530

    border radius added

--- public/js/app.js
+++ public/js/app.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 function fetchData() {
-  fetch("http://localhost:8000/dashboard/api/crypto-data")
+  fetch("https://quad-b-tech-backend-one.vercel.app/dashboard/api/crypto-data")
     .then((response) => response.json())
     .then((data) => {
       if (data.selectedCoin.length > 0) {
@@ -37,17 +37,23 @@ function populateTable(data) {
     }
 
     row.innerHTML = `
-    <td>${index + 1}</td>
-    <td>${item.name}</td>  
-    <td>₹${item.last}</td>  
-    <td>₹${item.buy} / ${item.sell}</td>  
-    <td style="color: ${item.last > item.open ? "#28a745" : "#dc3545"}">
-        <i class="fas fa-caret-${
-          item.last > item.open ? "up" : "down"
-        } icon_spacing"></i>
-        ${priceDifference}
-    </td>
-    <td>₹${item.volume}</td>  
+    <tr>
+            <td style="border-top-left-radius: 14px; border-bottom-left-radius: 14px;">${
+              index + 1
+            }</td>
+            <td>${item.name}</td>  
+            <td>₹${item.last}</td>  
+            <td>₹${item.buy} / ${item.sell}</td>  
+            <td style="color: ${item.last > item.open ? "#28a745" : "#dc3545"}">
+                <i class="fas fa-caret-${
+                  item.last > item.open ? "up" : "down"
+                } icon_spacing"></i>
+                ${priceDifference}
+            </td>
+            <td style="border-top-right-radius: 14px; border-bottom-right-radius: 14px;">₹${
+              item.volume
+            }</td>
+        </tr>
   `;
 
     tableBody.appendChild(row);
