246797e0bc63fb71ecf87ffc9d6f0d6c39adf0bd
Onkar Patil <onkarvgpatil1998@gmail.com>
2024-09-16 23:03:28 +0530

    frontend implemented

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const express = require("express");
 const app = express();
+const path = require("path");
+app.use(express.static(path.join(__dirname, "public")));
 app.use(express.json());
-app.use("/dashboard", require("./routes/dashboard/dashboard.js"));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+app.use("/dashboard/api", require("./routes/dashboard/dashboard.js"));
 
 app.listen(8000, () => {
   console.log("server port is listing on 8000");
---
+++ public/js/app.js
@@ -0,0 +1,46 @@
+document.addEventListener("DOMContentLoaded", function () {
+  fetchData();
+});
+
+function fetchData() {
+  fetch("http://localhost:8000/dashboard/api/crypto-data")
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data.selectedCoin);
+      populateTable(data.selectedCoin);
+    })
+    .catch((error) => console.error("Error fetching data:", error));
+}
+
+function populateTable(data) {
+  console.log(data);
+  const tableBody = document.getElementById("table-body");
+
+  tableBody.innerHTML = "";
+
+  data.forEach((item, index) => {
+    const row = document.createElement("tr");
+
+    let priceDifference = "N/A";
+    if (item.open && item.open > 0) {
+      priceDifference =
+        (((item.last - item.open) / item.open) * 100).toFixed(2) + "%";
+    }
+
+    row.innerHTML = `
+    <td>${index + 1}</td>
+    <td>${item.name}</td> <!-- Platform (e.g., "BAT/INR") -->
+    <td>${item.last}</td> <!-- Last Traded Price -->
+    <td>${item.buy} / ${item.sell}</td> <!-- Buy/Sell Price -->
+    <td style="color: ${item.last > item.open ? "#28a745" : "#dc3545"}">
+        <i class="fas fa-caret-${
+          item.last > item.open ? "up" : "down"
+        } icon_spacing"></i>
+        ${priceDifference}
+    </td>
+    <td>${item.volume}</td> <!-- Volume (as savings) -->
+  `;
+
+    tableBody.appendChild(row);
+  });
+}
